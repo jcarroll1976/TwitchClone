6b0b77ad60ac9e2d74a4b6ca92ddd027c352896d
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-12 20:43:07 -0400

    Added logo to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import logo from "../public/assets/logo.png"
+import Image from 'next/image';
+
+
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10'>
+      {/*Left Side*/}
+      <div className='flex grow items-center justify-start'>
+        <Link href='/'>
+            <Image src={logo} alt='/' width='36' height='36' className='cursor-pointer z-10' />
+        </Link>
+      </div>
+      {/*Middle*/}
+      {/*Right Side*/}
+    </div>
   )
 }
