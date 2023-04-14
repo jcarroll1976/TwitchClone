79a9f7561d66e4206f39a7094aefc15bb24a70a9
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-13 21:02:54 -0400

    Added Headless UI component to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link'
-import React from 'react'
+import React, {Fragment} from 'react'
 import logo from "../public/assets/logo.png"
 import Image from 'next/image';
+import { Menu, Transition } from '@headlessui/react';
+import {BsThreeDotsVertical} from "react-icons/bs";
+
+function classNames(...classes:string[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 
 
@@ -13,9 +19,78 @@ export default function Navbar() {
         <Link href='/'>
             <Image src={logo} alt='/' width='36' height='36' className='cursor-pointer z-10' />
         </Link>
+        <div className='p-4'>
+          <Menu as='div' className='relative text-left'>
+            <div className='flex'>
+              <Menu.Button>
+                <BsThreeDotsVertical size={20} />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Settings
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        Support
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href='#'
+                        className={classNames(
+                          active
+                            ? 'bg-gray-500 text-gray-100'
+                            : 'text-gray-200',
+                          'block px-4 py-2 text-sm'
+                        )}
+                      >
+                        License
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
       </div>
       {/*Middle*/}
       {/*Right Side*/}
-    </div>
+      </div>
   )
 }
